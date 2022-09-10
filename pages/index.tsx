4d7925096159b1982997bbcbc5fc0619b4b39165
adrea <90507395+adreaisbad@users.noreply.github.com>
2022-09-10 22:21:22 +0700

    v1: fix typo, mobile friendly page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { games } from "./data/games.json";
+import { PlayIcon } from "@heroicons/react/24/solid";
 
 const Home: NextPage = () => {
   return (
@@ -14,16 +15,16 @@ const Home: NextPage = () => {
         <div className="bg-gray-900 h-screen w-screen">
           <div className="py-10 container mx-auto">
             <h1 className="text-center text-6xl font-bold text-blue-800 ">
-              Collection of Games
+              Games
             </h1>
             <p className="text-center text-xl font-medium text-gray-400">
               Here's a small collection of some flash and fun games you can play
-              while in school.
+              while at school.
             </p>
           </div>
           <div className="py-6 md:flex justify-center grids lg:grid-cols-4">
             {games.map((game) => (
-              <div className="ml-3 mr-4 w-96 h-64 bg-gray-800 rounded-lg">
+              <div className="ml-3 mr-4 w-96 h-auto bg-gray-800 rounded-lg">
                 <img
                   src={game.image}
                   alt={game.id}
@@ -34,14 +35,15 @@ const Home: NextPage = () => {
                   <h1 className="mt-3 ml-5 text-white text-3xl font-bold">
                     {game.title}
                   </h1>
-                  <p className="mt-7 ml-2 bg-orange-500 text-orange-100 w-16 h-4 text-center text-xs rounded-lg shadow">
+                  <p className="mt-7 ml-2 bg-orange-500 text-orange-100 w-8 h-4 text-center text-xs rounded-lg shadow">
                     Fun
                   </p>
                 </div>
 
                 <p className="ml-5 text-gray-400">{game.description}</p>
-                <button className="ml-5 mt-2 w-32 h-8 text-bold text-white text-sm bg-blue-600 rounded-lg shadow">
-                  Play Now
+                <button className="ml-5 mt-2 w-32 h-8 text-bold text-white flex text-sm bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg shadow">
+                  <PlayIcon className="ml-5 mt-1 w-6 h-5" />
+                  <p className="mt-[5px]">Play Now</p>
                 </button>
               </div>
             ))}
