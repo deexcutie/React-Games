5d18fddf004cbd6e535b3179de729c894c136b97
adrea <90507395+adreaisbad@users.noreply.github.com>
2022-09-10 21:22:18 +0700

    v1: testing game

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { games } from "./data/games.json";
 
 const Home: NextPage = () => {
   return (
@@ -12,15 +12,22 @@ const Home: NextPage = () => {
 
       <main className="">
         <div className="bg-gray-900 h-screen w-screen">
-          <div className="container mx-auto">
-            <h1 className="text-center text-6xl font-extrabold text-blue-800 ">
-              Games
+          <div className="py-10 container mx-auto">
+            <h1 className="text-center text-6xl font-bold text-blue-800 ">
+              Collection of Games
             </h1>
-            <p className="text-center text-xl font-semibold text-gray-400">
+            <p className="text-center text-xl font-medium text-gray-400">
               Here's a small collection of some flash and fun games you can play
               while in school.
             </p>
           </div>
+          {games.map((game) => (
+            <div className="px-6 flex grids grid-cols-4 gap-4">
+              <div className="w-96 h-64 bg-gray-800">
+                <h1>{game.title}</h1>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
 
@@ -31,8 +38,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <b>dxqt.lol</b>
+          Powered by <b>dxqt.lol</b>
         </a>
       </footer>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
       },
     },
   },
