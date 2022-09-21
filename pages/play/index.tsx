aaf49534175d87df063234aa3791e6d23035757b
dxqt <90507395+deexcutie@users.noreply.github.com>
2022-09-21 19:15:20 +0700

    Update index.tsx

--- pages/play/index.tsx
+++ pages/play/index.tsx
@@ -1,19 +1,13 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Display from '../../components/display'
-
-// Here you would fetch and return the user
-const useUser = () => ({ user: null, loading: false })
 
 export default function Redirect() {
-  const { user, loading } = useUser()
   const router = useRouter()
 
   useEffect(() => {
-    if (!(user || loading)) {
       router.push('/')
     }
-  }, [user, loading])
+  }, [])
 
   return
 }
