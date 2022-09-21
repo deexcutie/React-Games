6459c1c5c959cd74a57f4fc5858cafa07b53bdc5
adrea <90507395+deexcutie@users.noreply.github.com>
2022-09-21 21:44:23 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,13 +19,13 @@ const Home: NextPage = () => {
               placeholder="Search..."
               name="s"
               onChange={(e) => setQuery(e.target.value)}
-              className="mb-7 focus:outline-none rounded-lg bg-gray-800 text-white py-2 px-3 w-72"
+              className="mb-7 focus:outline-none rounded-lg bg-slate-800 text-white py-2 px-3 w-72"
             />
           </div>
         </form>
         <ul
           role="list"
-          className="ml-8 grid gap-y-4 sm:grid-cols-2 sm:gap-x-10 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 place-items-center"
+          className="ml-8 grid gap-y-4 sm:grid-cols-2 sm:gap-x-10 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 place-items-center"
         >
           {games
             .filter((game) => {
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
             })
             .map((game, index) => (
               <li key={index} className="relative">
-                <div className="group block ml-3 mr-4 md:w-96 lg:w-96 xl:w-80 2xl:w-96 h-auto bg-gray-800 rounded-lg">
+                <div className="group block ml-3 mr-4 md:w-96 lg:w-96 xl:w-80 2xl:w-96 h-auto bg-slate-800 rounded-lg">
                   <img
                     src={game.image}
                     alt={game.title}
@@ -52,7 +52,7 @@ const Home: NextPage = () => {
                       {game.author}
                     </p> */}
 
-                  <p className="block ml-5 text-gray-400">{game.description}</p>
+                  <p className="block ml-5 text-slate-400">{game.description}</p>
                   <button
                     onClick={() => router.push("/normal/play/" + game.id)}
                     className="ml-5 mt-2 w-32 h-8 text-bold text-white flex text-sm bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg shadow"
